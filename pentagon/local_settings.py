d7f76a2542e2032df82656a935352097500f1c88
James Turk <james.p.turk@gmail.com>
2012-01-18 16:43:19 -0500

    more config

--- pentagon/local_settings.py
+++
@@ -1,9 +0,0 @@
-DATABASES = {
-    'default': {
-        'NAME': 'pentagon',
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'HOST': 'localhost',
-        'USER': 'pentagon',
-        'PASSWORD': 'test',
-    }
-}
