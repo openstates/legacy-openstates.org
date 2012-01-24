e0841e105a851991d5c2ad2d0d9c4fc9069993b3
James Turk <james.p.turk@gmail.com>
2012-01-24 13:41:18 -0500

    fix downloads

--- pentagon/downloads.py
+++ pentagon/downloads.py
@@ -4,7 +4,7 @@ import shutil
 
 
 def download_census_shapefiles(slug, url, download=True):
-    dirname = 'downloads/{0}/'.format(slug)
+    dirname = 'shapefiles/{0}/'.format(slug)
     if download:
         os.makedirs(dirname)
     os.chdir(dirname)
@@ -14,13 +14,14 @@ def download_census_shapefiles(slug, url, download=True):
     # unzip
     for f in glob.glob('*.zip'):
         os.system('unzip -o %s' % f)
+        os.system('rm %s' % f)
 
     os.chdir('../..')
 
 
-process_census_shapefiles('sldu',
+download_census_shapefiles('sldu',
   'ftp://ftp2.census.gov/geo/tiger/TIGER2010/SLDU/2010/*.zip')
-process_census_shapefiles('sldl',
+download_census_shapefiles('sldl',
   'ftp://ftp2.census.gov/geo/tiger/TIGER2010/SLDL/2010/*.zip')
 #process_census_shapefiles('cd',
 #  'ftp://ftp2.census.gov/geo/tiger/TIGER2010/CD/111/tl_2010_us_cd111.zip')
