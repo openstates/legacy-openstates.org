486f309ed5289bb09c786ffd800e9923b559c22c
James Turk <james.p.turk@gmail.com>
2012-11-16 11:48:44 -0500

    move everything up a dir

--- createdb.sh
+++
@@ -1,4 +0,0 @@
-createuser pentagon -P
-createdb pentagon
-psql -d pentagon -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-psql -d pentagon -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql 
