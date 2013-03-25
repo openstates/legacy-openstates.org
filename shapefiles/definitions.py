81796792f0cde546dc755ddfb906f165618dfd7c
Paul Tagliamonte <paultag@sunlightfoundation.com>
2013-03-25 14:45:56 -0400

    fixing up the namer

--- shapefiles/definitions.py
+++ shapefiles/definitions.py
@@ -4,24 +4,35 @@ Configuration describing the shapefiles to be loaded.
 from datetime import date
 import boundaries
 
+
+state_fips = {
+    '01': 'al', '02': 'ak', '04': 'az', '05': 'ar', '06': 'ca', '08': 'co',
+    '09': 'ct', '10': 'de', '11': 'dc', '12': 'fl', '13': 'ga', '15': 'hi',
+    '16': 'id', '17': 'il', '18': 'in', '19': 'ia', '20': 'ks', '21': 'ky',
+    '22': 'la', '23': 'me', '24': 'md', '25': 'ma', '26': 'mi', '27': 'mn',
+    '28': 'ms', '29': 'mo', '30': 'mt', '31': 'ne', '32': 'nv', '33': 'nh',
+    '34': 'nj', '35': 'nm', '36': 'ny', '37': 'nc', '38': 'nd', '39': 'oh',
+    '40': 'ok', '41': 'or', '42': 'pa', '44': 'ri', '45': 'sc', '46': 'sd',
+    '47': 'tn', '48': 'tx', '49': 'ut', '50': 'vt', '51': 'va', '53': 'wa',
+    '54': 'wv', '55': 'wi', '56': 'wy', '60': 'as', '66': 'gu', '69': 'mp',
+    '72': 'pr', '78': 'vi'
+}
+
+
 def tiger_namer(feature):
-    state_fips = {
-        '01': 'al', '02': 'ak', '04': 'az', '05': 'ar', '06': 'ca', '08': 'co',
-        '09': 'ct', '10': 'de', '11': 'dc', '12': 'fl', '13': 'ga', '15': 'hi',
-        '16': 'id', '17': 'il', '18': 'in', '19': 'ia', '20': 'ks', '21': 'ky',
-        '22': 'la', '23': 'me', '24': 'md', '25': 'ma', '26': 'mi', '27': 'mn',
-        '28': 'ms', '29': 'mo', '30': 'mt', '31': 'ne', '32': 'nv', '33': 'nh',
-        '34': 'nj', '35': 'nm', '36': 'ny', '37': 'nc', '38': 'nd', '39': 'oh',
-        '40': 'ok', '41': 'or', '42': 'pa', '44': 'ri', '45': 'sc', '46': 'sd',
-        '47': 'tn', '48': 'tx', '49': 'ut', '50': 'vt', '51': 'va', '53': 'wa',
-        '54': 'wv', '55': 'wi', '56': 'wy', '60': 'as', '66': 'gu', '69': 'mp',
-        '72': 'pr', '78': 'vi'
-    }
-
-    fips_code = feature.get('STATEFP')
+    global state_fips
+    fips_code = feature.get("STATEFP")
     state_abbrev = state_fips[fips_code].upper()
-
     return "%s %s" % (state_abbrev, feature.get('NAME'))
+
+
+def tiger10_namer(feature):
+    global state_fips
+    fips_code = feature.get("STATEFP10")
+    state_abbrev = state_fips[fips_code].upper()
+    return "%s %s" % (state_abbrev, feature.get('NAMELSAD10'))
+
+
 
 class index_namer(object):
     def __init__(self, prefix):
@@ -98,7 +109,7 @@ boundaries.register('county',
                     domain='United States',
                     file='county/',
                     last_updated=date(2012, 11, 15),
-                    name_func=tiger_namer,
+                    name_func=tiger10_namer,
                     id_func=index_namer('county-'),
                     authority='US Census Bureau',
                     source_url=CENSUS_URL,
