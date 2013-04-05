3b68b25767621b03284234ae62f424516568963c
James Turk <james.p.turk@gmail.com>
2013-04-05 16:17:36 -0400

    add locust dependency

--- settings/base.py
+++ settings/base.py
@@ -7,17 +7,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 TIME_ZONE = 'America/New_York'
 LANGUAGE_CODE = 'en-us'
 SITE_ID = 1
@@ -55,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sites',
     'boundaries',
+    'locust',
 )
 
 LOGGING = {
@@ -99,6 +89,3 @@ LOGGING = {
 }
 
 BOUNDARIES_SHAPEFILES_DIR = 'shapefiles'
-
-from local_settings import *
-
--- urls.py
+++ urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 
+#from django.contrib import admin
+#admin.autodiscover()
+
 urlpatterns = patterns('',
    (r'', include('boundaries.urls')),
+   (r'^locust/', include('locust.urls')),
+   #(r'^admin/', include(admin.site.urls)),
 )
