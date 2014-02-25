97d855c504d7f88755db501d2be5c673d212ee89
James Turk <james.p.turk@gmail.com>
2014-02-25 15:06:16 -0500

    add redirect to docs

--- ocdapi/urls.py
+++ ocdapi/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include
+from django.views.generic.base import RedirectView
 
 #from django.contrib import admin
 #admin.autodiscover()
 
 urlpatterns = patterns('',
+   ('^$', RedirectView.as_view(url='http://docs.opencivicdata.org/en/latest/api/index.html')),
    ('', include('boundaries.urls')),
    ('', include('imago.urls')),
    ('', include('locksmith.mongoauth.urls')),
