8db50b9ad7e44752d52ca224e0f50c5791822362
James Turk <james.p.turk@gmail.com>
2014-04-03 17:42:21 -0400

    cheerwine updates

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import env, task
 from cheerwine.server import set_hosts, install_base, write_cron
 from cheerwine.roles import Django, Postgres
-from cheerwine.config import config
+from cheerwine.tasks import production
 
 env.use_ssh_config = True
 
@@ -15,14 +15,15 @@ def cron():
     write_cron('0 4 * * * /projects/openstates/virt/bin/python /projects/openstates/src/openstates/site/manage.py apireport >> /projects/openstates/logs/apireport.log', 'openstates')
 
 
-openstates = Django(name='openstates', ebs_size=10, wsgi_module='openstates.wsgi:application',
+openstates = Django(name='openstates',
+                    ebs_size=10,
+                    wsgi_module='openstates.wsgi:application',
                     repos={'openstates': 'git://github.com/sunlightlabs/openstates.org.git'},
-                    files={'/projects/openstates/src/openstates/openstates/settings/production.py': 'production.py',
-                           '/projects/openstates/src/openstates/billy_local.py': 'billy_local.py'
+                    files={'/projects/openstates/src/openstates/openstates/settings/production.py': 'settings.py',
+                           '/projects/openstates/src/openstates/billy_local.py': 'billy_local.py',
                           },
                     nginx_locations={'/robots.txt': '/projects/openstates/src/openstates/',
-                                     '/favicon.ico': '/projects/openstates/src/openstates/media/images/',
-                              },
+                                     '/favicon.ico': '/projects/openstates/src/openstates/media/images/', },
                     uwsgi_extras={'processes': 12,
                                   'reload-on-rss': 200,
                                   'log-x-forwarded-for': 'true'
@@ -32,6 +33,7 @@ openstates = Django(name='openstates', ebs_size=10, wsgi_module='openstates.wsgi
                                   # 'disable-logging': 'true',
                                  },
                     python3=False,
+                    server_name='openstates.org',
                     dependencies=['-r openstates/requirements.txt'],
                     django_settings='openstates.settings.production'
                    )
