1e362382f4242cdb9cd0b72271aae5bf8d0bb2e4
twneale <twneale@gmail.com>
2014-07-11 15:08:31 -0400

    Enabled admin in url conf.

--- ocdapi/urls.py
+++ ocdapi/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include
 from django.views.generic.base import RedirectView
 
-#from django.contrib import admin
-#admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
    ('^$', RedirectView.as_view(url='http://docs.opencivicdata.org/en/latest/api/index.html')),
    ('', include('boundaries.urls')),
    ('', include('imago.urls')),
    ('', include('locksmith.mongoauth.urls')),
-   #(r'^admin/', include(admin.site.urls)),
+   (r'^admin/', include(admin.site.urls)),
 )
