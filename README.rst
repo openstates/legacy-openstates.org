fce6425bb6c086b96ea507dfbf75e3a8ccf8e2f7
James Turk <james.p.turk@gmail.com>
2016-09-25 23:43:01 -0400

    more cleanup

--- README.rst
+++ README.rst
@@ -4,10 +4,3 @@ for something else:
 * `Open States Scrapers <https://github.com/sunlightlabs/openstates/>`_
 * `Open States API <http://openstates.org/api/>`_
 * `Open State Project Google Group <http://groups.google.com/group/fifty-state-project>`_
-
-Installation
-============
-
-fab prepare_server
-fab openstates_install_app
-fab openstates_install_server
--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,6 @@
-                    wsgi_module='openstates.wsgi:application',
-                    nginx_locations={'/robots.txt': '/projects/openstates/src/openstates/',
-                                     '/favicon.ico': '/projects/openstates/src/openstates/media/images/', },
-                    uwsgi_extras={'processes': 12,
-                                  'reload-on-rss': 200,
-                                  'log-x-forwarded-for': 'true'
-                                 },
+nginx_locations={'/robots.txt': '/projects/openstates/src/openstates/',
+'/favicon.ico': '/projects/openstates/src/openstates/media/images/', },
+uwsgi_extras={'processes': 12,
+'reload-on-rss': 200,
+'log-x-forwarded-for': 'true'
+},
