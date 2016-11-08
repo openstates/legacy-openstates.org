bc346bdfcdd31cc38e87001f6ce676ccafd07570
James Turk <james.p.turk@gmail.com>
2016-11-08 11:43:18 -0500

    emergency banner

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,3 @@
-nginx_locations={'/robots.txt': '/projects/openstates/src/openstates/',
-'/favicon.ico': '/projects/openstates/src/openstates/media/images/', },
 uwsgi_extras={'processes': 12,
 'reload-on-rss': 200,
 'log-x-forwarded-for': 'true'
