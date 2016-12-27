a560471dd3274ba12d0c343603a7903b2be2af81
James Turk <james.p.turk@gmail.com>
2016-12-27 00:07:21 -0500

    cached template loader

--- openstates/settings.py
+++ openstates/settings.py
@@ -52,10 +52,11 @@ STATICFILES_FINDERS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
