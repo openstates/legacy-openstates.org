36b653d1bff0f0ee30ea3435ce54f219452d8f1f
James Turk <james.p.turk@gmail.com>
2017-01-29 16:31:51 -0500

    improve donations form

--- donations/views.py
+++ donations/views.py
@@ -1,26 +1,39 @@
 import os
 import stripe
 from django.views.decorators.http import require_POST
+from django.shortcuts import render
 
 
-@require_POST
 def donate(request):
-    stripe.api_key = os.environ['STRIPE_API_KEY']
+    success = False
 
-    customer = stripe.Customer.create(
-        source=request.POST['stripeToken'],
-        email=request.POST['email'],
-    )
-    if 'plan' in request.POST:
-        subscription = stripe.Subscription.create(
-            customer=customer,
-            plan=request.POST['plan'],
+    if request.method == 'POST':
+        stripe.api_key = os.environ['STRIPE_API_KEY']
+
+        metadata = {
+            'source': request.POST.get('source', ''),
+            'donor_name': request.POST.get('donor_name', ''),
+        }
+
+        customer = stripe.Customer.create(
+            source=request.POST['stripeToken'],
+            email=request.POST['email'],
         )
-    else:
-        # just create a one-time charge
-        charge = stripe.Charge.create(
-            customer=customer,
-            amount=request.POST['amount'],
-            currency="usd",
-            description="Open States Donation",
-        )
+        if 'plan' in request.POST:
+            subscription = stripe.Subscription.create(
+                customer=customer,
+                plan=request.POST['plan'],
+                metadata=metadata,
+            )
+        else:
+            # just create a one-time charge
+            charge = stripe.Charge.create(
+                customer=customer,
+                amount=request.POST['amount'],
+                currency="usd",
+                description="Open States Donation",
+                metadata=metadata,
+            )
+        success = True
+
+    return render(request, 'flat/donate.html', {'success': success})
