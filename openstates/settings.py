c944fe83801708d40f8d269ac13730cdb08b6b2f
James Turk <james.p.turk@gmail.com>
2017-04-20 23:40:19 -0400

    email settings

--- openstates/settings.py
+++ openstates/settings.py
@@ -6,6 +6,12 @@ if os.environ.get('DEBUG', 'true').lower() == 'false':
     TEMPLATE_DEBUG = DEBUG
     ALLOWED_HOSTS = ['*']
     SECRET_KEY = os.environ['SECRET_KEY']
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = os.environ['EMAIL_HOST']
+    EMAIL_PORT = os.environ['EMAIL_PORT']
+    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+    EMAIL_USE_TLS = True
 else:
     DEBUG = True
     ALLOWED_HOSTS = ['*']
