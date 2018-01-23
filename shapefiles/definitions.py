ccb1a1fff46f3e201e01403039c088f85c112b20
Miles Watkins <miles.w.watkins@gmail.com>
2018-01-23 14:52:41 -0500

    Switch to easier-to-understand enumeration of FIPS

--- shapefiles/definitions.py
+++ shapefiles/definitions.py
@@ -5,20 +5,10 @@ from django.contrib.gis.gdal.error import OGRIndexError
 from datetime import date
 import boundaries
 import os
+import us
 
-
-state_fips = {
-    '01': 'al', '02': 'ak', '04': 'az', '05': 'ar', '06': 'ca', '08': 'co',
-    '09': 'ct', '10': 'de', '11': 'dc', '12': 'fl', '13': 'ga', '15': 'hi',
-    '16': 'id', '17': 'il', '18': 'in', '19': 'ia', '20': 'ks', '21': 'ky',
-    '22': 'la', '23': 'me', '24': 'md', '25': 'ma', '26': 'mi', '27': 'mn',
-    '28': 'ms', '29': 'mo', '30': 'mt', '31': 'ne', '32': 'nv', '33': 'nh',
-    '34': 'nj', '35': 'nm', '36': 'ny', '37': 'nc', '38': 'nd', '39': 'oh',
-    '40': 'ok', '41': 'or', '42': 'pa', '44': 'ri', '45': 'sc', '46': 'sd',
-    '47': 'tn', '48': 'tx', '49': 'ut', '50': 'vt', '51': 'va', '53': 'wa',
-    '54': 'wv', '55': 'wi', '56': 'wy', '60': 'as', '66': 'gu', '69': 'mp',
-    '72': 'pr', '78': 'vi'
-}
+states = [s for s in us.STATES_AND_TERRITORIES if s not in us.OBSOLETE]
+state_fips = {s.fips: s.abbr.lower() for s in states}
 
 
 def tiger_namer(feature):
